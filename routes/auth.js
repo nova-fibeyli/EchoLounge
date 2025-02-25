1f5e2dee6f98fe1cff0f32962a8c8a3446e12725
abeiseke <aika.b.2005@gmail.com>
2025-02-25 14:53:07 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const passport = require('passport');
 const User = require('../models/User');
 const router = express.Router();
 
-const ADMIN_EMAILS = ['admin@example.com', 'admin2@example.com']; // Список email, которым даётся роль "admin"
+const ADMIN_EMAILS = ['admin@example.com', 'admin2@example.com']; 
 
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    // Проверяем, есть ли email в списке админов
     const role = ADMIN_EMAILS.includes(email) ? 'admin' : 'user';
 
     const user = await User.create({ username, email, password: hashedPassword, role });
@@ -30,18 +28,6 @@ router.post('/login', async (req, res) => {
   }
   const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
   res.json({ token });
-});
-
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
-  const token = jwt.sign(
-    { id: req.user._id, role: req.user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: '1h' }
-  );
-
-  res.redirect(`/success?token=${token}`);
 });
 
 module.exports = router;
